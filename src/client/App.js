fae27bd3003edf241e968a6e5156f2364090ddbc
Ben Katz <bkatz@fb.com>
2020-01-17 09:51:01 +0000

    styling and refactor

--- src/client/App.js
+++ src/client/App.js
@@ -1,81 +1,57 @@
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, {
+  useState,
+  useEffect
+} from 'react';
 
 import {ResponsiveLineCanvas} from '@nivo/line';
 
 const refresh = onDone => {
-  return fetch('/api/fit').then(async response => {
-    if (response.ok) {
-      const responseData = await response.json();
-      onDone(responseData);
-    } else {
-      throw Error(`${response.status}: ${response.statusText}`);
+  return fetch('/api/fit').then(
+    async response => {
+      if (response.ok) {
+        const responseData = await response.json();
+        onDone(responseData);
+      } else {
+        throw Error(
+          `${response.status}: ${response.statusText}`
+        );
+      }
     }
-  });
+  );
 };
 export default function App() {
-  // const data = [
-  //   {
-  //     id: 'First',
-  //     data: [
-  //       {x: 1, y: 2},
-  //       {x: 3, y: 4}
-  //     ]
-  //   }
-  // ];
-
   const [data, setData] = useState([]);
-  const [file, setFile] = useState();
   useEffect(() => {
     refresh(setData);
-  }, [file]);
+  }, []);
 
+  console.log({data});
   return (
     <span className="app-container">
-      <span className="file-uploader">
-        <input
-          type="file"
-          name="file"
-          accept=".fit"
-          onChange={event => {
-            setFile(event.target.files[0]);
-          }}
-        />
-        <button
-          type="button"
-          onClick={() => {
-            if (file !== null) {
-              const formData = new FormData();
-              formData.append('file', file);
-              fetch('/api/upload', {
-                // Your POST endpoint
-                method: 'POST',
-                body: formData // This is your file object
-              })
-                // .then(response => response.json())
-                .then(success => {
-                  console.log(success); // Handle the success response object
-                  refresh(setData);
-                })
-                .catch(
-                  error => console.log(error) // Handle the error response object
-                );
-            } else {
-              console.error('Tried to upload an empty file');
-            }
-          }}>
-          Upload
-        </button>
-      </span>
+      <FileUploader
+        onSetFileData={setData}
+      />
       {data.length > 0 ? (
         <ResponsiveLineCanvas
           data={data}
+          yScale={{
+            type: 'linear',
+            min: 'auto'
+          }}
+          curve="cardinal"
+          enablePoints={false}
           xScale={{
             type: 'linear'
             // format: '%s',
             // precision: 'second'
           }}
-          margin={{top: 50, right: 50, bottom: 50, left: 50}}
+          margin={{
+            top: 50,
+            right: 50,
+            bottom: 50,
+            left: 50
+          }}
           // gridXValues={[0, 20, 40, 60, 80, 100, 120]}
           // gridYValues={[60, 80, 100, 120, 140, 160, 180]}
           axisBottom={{
@@ -93,3 +69,54 @@ export default function App() {
     </span>
   );
 }
+
+const FileUploader = ({
+  onSetFileData
+}) => {
+  const [file, setFile] = useState();
+  return (
+    <span className="file-uploader">
+      <input
+        type="file"
+        name="file"
+        accept=".fit"
+        onChange={event => {
+          setFile(
+            event.target.files[0]
+          );
+        }}
+      />
+      <button
+        type="button"
+        onClick={() => {
+          if (file !== null) {
+            const formData = new FormData();
+            formData.append(
+              'file',
+              file
+            );
+            fetch('/api/upload', {
+              // Your POST endpoint
+              method: 'POST',
+              body: formData // This is your file object
+            })
+              // .then(response => response.json())
+              .then(success => {
+                console.log(success); // Handle the success response object
+                refresh(onSetFileData);
+              })
+              .catch(
+                error =>
+                  console.log(error) // Handle the error response object
+              );
+          } else {
+            console.error(
+              'Tried to upload an empty file'
+            );
+          }
+        }}>
+        Upload
+      </button>
+    </span>
+  );
+};
--- src/server/index.js
+++ src/server/index.js
@@ -3,16 +3,26 @@ const bodyParser = require('body-parser');
 const fs = require('fs');
 // const cors = require('cors');
 const multer = require('multer');
+
 const storage = multer.memoryStorage();
-const upload = multer({storage, fileSize: 1000});
+const upload = multer({
+  storage,
+  fileSize: 1000
+});
 
 const app = express();
 // const gpxParse = require('gpx-parse');
-const FitParser = require('fit-file-parser').default;
+const FitParser = require('fit-file-parser')
+  .default;
 
 let savedFile = null;
 
-function fitParse(content, next, req, res) {
+function fitParse(
+  content,
+  next,
+  req,
+  res
+) {
   const fitParser = new FitParser({
     force: true,
     speedUnit: 'km/h',
@@ -22,34 +32,47 @@ function fitParse(content, next, req, res) {
     mode: 'cascade'
   });
 
-  return fitParser.parse(content, (error, data) => {
-    // Handle result of parse method
-    if (error) {
-      next(error);
-    } else {
-      // res.send(
-      //   `${JSON.stringify(
-      //     data.activity.sessions[0].laps[0].records,
-      //     null,
-      //     2
-      //   )}`
-      // );
+  return fitParser.parse(
+    content,
+    (error, data) => {
+      // Handle result of parse method
+      if (error) {
+        next(error);
+      } else {
+        // res.send(
+        //   `${JSON.stringify(
+        //     data.activity.sessions[0].laps[0].records,
+        //     null,
+        //     2
+        //   )}`
+        // );
 
-      const {records} = data.activity.sessions[0].laps[0];
+        const {
+          records
+        } = data.activity.sessions[0].laps[0];
 
-      const heartBeats = records.map(record => ({
-        y: record.heart_rate,
-        x: record.timer_time
-      }));
-      const chartData = [
-        {
-          id: 'heart_rate',
-          data: heartBeats
-        }
-      ];
-      res.send(JSON.stringify(chartData, null, 2));
+        const heartBeats = records.map(
+          record => ({
+            y: record.heart_rate,
+            x: record.timer_time
+          })
+        );
+        const chartData = [
+          {
+            id: 'heart_rate',
+            data: heartBeats
+          }
+        ];
+        res.send(
+          JSON.stringify(
+            chartData,
+            null,
+            2
+          )
+        );
+      }
     }
-  });
+  );
 }
 
 app.use(express.static('dist'));
@@ -71,27 +94,49 @@ const path = './test2.gpx';
 //     }
 //   });
 // });
-app.get('/api/fit', async (req, res, next) => {
-  const fitPath = './test.fit';
-  if (!fs.existsSync(fitPath)) {
-    next(`The file ${fitPath} doesn't exist`);
+app.get(
+  '/api/fit',
+  async (req, res, next) => {
+    const fitPath = './test.fit';
+    if (!fs.existsSync(fitPath)) {
+      next(
+        `The file ${fitPath} doesn't exist`
+      );
+    }
+    if (savedFile != null) {
+      return fitParse(
+        savedFile.buffer,
+        next,
+        req,
+        res
+      );
+    }
+    return fs.readFile(
+      fitPath,
+      (err, content) => {
+        return fitParse(
+          content,
+          next,
+          req,
+          res
+        );
+      }
+    );
   }
-  if (savedFile != null) {
-    return fitParse(savedFile.buffer, next, req, res);
-  }
-  return fs.readFile(fitPath, (err, content) => {
-    return fitParse(content, next, req, res);
-  });
-});
+);
 
-app.post('/api/upload', upload.single('file'), async (req, res, next) => {
-  if (req.file !== undefined) {
-    savedFile = req.file;
-    res.end();
-  } else {
-    next('Body was empty');
+app.post(
+  '/api/upload',
+  upload.single('file'),
+  async (req, res, next) => {
+    if (req.file !== undefined) {
+      savedFile = req.file;
+      res.end();
+    } else {
+      next('Body was empty');
+    }
   }
-});
+);
 
 // app.post('/api/todos', async (req, res, next) => {
 //   if (req.body === undefined) {
@@ -109,6 +154,11 @@ app.post('/api/upload', upload.single('file'), async (req, res, next) => {
 //   res.end();
 // });
 
-app.listen(process.env.PORT || 8080, () =>
-  console.log(`Listening on port ${process.env.PORT || 8080}!`)
+app.listen(
+  process.env.PORT || 8080,
+  () =>
+    console.log(
+      `Listening on port ${process.env
+        .PORT || 8080}!`
+    )
 );
